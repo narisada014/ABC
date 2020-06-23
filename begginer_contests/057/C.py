2ef7edcbbdf58c226d19b4b14aa4ce5de2aebcbf
narisada014 <nari.reishi@gmail.com>
2020-06-24 02:14:13 +0900

    add 057

---
+++ begginer_contests/057/C.py
@@ -0,0 +1,37 @@
+N = int(input())
+# 約数羅列
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+
+    divisors.sort()
+    return divisors
+div_arr = make_divisors(N)
+
+if len(div_arr) == 1:
+    print(1)
+elif len(div_arr) == 2:
+    ans_num = max(div_arr)
+    print(len(str(ans_num)))
+else:
+    memo = []
+    if len(div_arr) % 2 == 0:
+        last_idx = -1
+        count = 0
+        for i in range(len(div_arr)):
+            count += 1
+            if N == div_arr[i] * div_arr[last_idx] and count != len(div_arr) // 2:
+                last_idx -= 1
+                memo.append(len(str(div_arr[last_idx])))
+            else:
+                break
+    else:
+        target_index = len(div_arr) // 2
+        memo.append(len(str(div_arr[target_index])))
+    print(min(memo))
+
+
--- beginer_contests/170/a.py
+++
@@ -1,6 +0,0 @@
-a = input().split()
-index = 0
-for i in a:
-    index += 1
-    if i == '0':
-        print(index)
