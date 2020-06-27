8ac913dd2ba530a318016f09c2cfdfc8e340f91c
narisada014 <nari.reishi@gmail.com>
2020-06-28 01:22:54 +0900

    fix 172 C

--- begginer_contests/172/C.py
+++ begginer_contests/172/C.py
@@ -11,19 +11,22 @@ for i in range(M):
 
 ans = 0
 for a_count in range(N+1):
+    # 積読消化時間read_time_a[a_count]がKより大きい場合は処理をスルー
+    if read_time_a[a_count] > K:
+        continue
     a_read_time = read_time_a[a_count]
     max_b_count = 0
     r = len(read_time_b)
     # 消化時間の配列はソートされている状態だから二分探索
+    # 計算量をlog(M)に削減
     while max_b_count + 1 < r:
-        # 2分探索で計算量をlog(r)に削減
         c = (max_b_count + r) // 2
+        # bがインデックスcで時間がaの時間+bの時間でK以下ならmax_b_countを二分探索の開始位置に更新する。
         if a_read_time + read_time_b[c] <= K:
             max_b_count = c
-        # 超えちゃったらwhileをr=cでストップ
+        # 超えちゃったら二分探索の前方に探索領域の終端を絞る
         else:
             r = c
-    # 積読消化時間がKより小さく、a_count + max_b_countの最大数が最大になるものをansに代入し続ける
-    if read_time_a[a_count] + read_time_b[max_b_count] <= K:
-        ans = max(ans, a_count + max_b_count)
+    # a_count + max_b_countの最大数が最大になるものをansに代入し続ける
+    ans = max(ans, a_count + max_b_count)
 print(ans)
