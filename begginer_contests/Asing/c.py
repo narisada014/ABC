58dc7dff70e65152fdfb37e032e63326d95809a3
narisada014 <nari.reishi@gmail.com>
2020-07-11 23:14:20 +0900

    compare good and bad example

--- begginer_contests/Asing/c.py
+++ begginer_contests/Asing/c.py
@@ -1,3 +1,29 @@
+# クソコード例
+# x,y,zの取りうる範囲は1 ≤ x, y, z ≤ n^(1/2)程度なので105までくらい
+# 最後に100 * 100 * 100をするのはいいが、これは各nに対して計算を回すことになるので
+# 100000とか来たら100 * 100 * 100 * 100000になって無事死亡
+N = int(input())
+def shiki(a, b, c):
+    return a ** 2 + b ** 2 + c ** 2 + a * b + b * c + c * a
+
+def condition(n):
+    count = 0
+    i = 0
+    j = 0
+    k = 0
+    for i in range(1, 105):
+        for j in range(1, 105):
+            for k in range(1, 105):
+                if shiki(i, j, k) == n:
+                    count += 1
+    print(count)
+
+for i in range(N):
+    condition(i)
+
+# いいコード例
+# 先に配列を作っておいて、100 * 100 * 100の計算一回で全ての個数を記録しておき、
+# インデックスアクセスで値を標準出力する
 N = int(input())
 ans = [0] * 100000
 for x in range(1, 100):
